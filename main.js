268a3396542f788ac909a27a5c3045d4ae64923a
ScrubC0der <rorkedraws@gmail.com>
2024-05-09 18:32:48 -0500

    Add input validation error handling

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
+import { ERRORS } from "./src/lib";
+
+console.log(ERRORS);
+
 const formEls = [
   ...Array.from(document.querySelectorAll("input")),
   document.querySelector("#message"),
 ];
-const submitBtn = document.querySelector("#submit");
-const sbmitBtn = document.querySelector("button[type='submit']");
+
+const submitBtn = document.querySelector("button[type='submit']");
 
 formEls.forEach((el) => {
   el.addEventListener("blur", (e) => {
-    console.log("blur event", e.target.id);
+    console.log(
+      ERRORS.find((error) => error.id === e.target.id).validate(e.target.value),
+    );
   });
 });
